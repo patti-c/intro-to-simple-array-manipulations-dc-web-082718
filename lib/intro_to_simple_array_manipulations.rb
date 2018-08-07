531a39edbe2aa7e1d6ee37ee161943a1db7ba647
Patricia Carter <patricia4194@gmail.com>
2018-08-07 17:10:58 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_push(array, string)
-  
+  array.push(string)
 end 
 
 def using_unshift(array, string)
@@ -39,3 +39,6 @@ def using_flatten(array)
 end 
 
 def using_delete(array, string)
+  
+end 
+
